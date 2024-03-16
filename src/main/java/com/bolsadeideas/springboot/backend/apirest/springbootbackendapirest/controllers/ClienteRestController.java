cbc91885c0d0c3d8d7a114bca86192da39ad0a08
ChrisferDev <ferchris82@hotmail.com>
2024-03-15 22:39:34 -0500

    Versión 1.3

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/springbootbackendapirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/springbootbackendapirest/controllers/ClienteRestController.java
@@ -1,9 +1,13 @@
 package com.bolsadeideas.springboot.backend.apirest.springbootbackendapirest.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bolsadeideas.springboot.backend.apirest.springbootbackendapirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.springbootbackendapirest.models.services.IClienteService;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
@@ -27,25 +31,42 @@ public class ClienteRestController {
     private IClienteService clienteService;
 
     @GetMapping("/clientes")
-    public List<Cliente> index(){
-        return clienteService.findAll();    
+    public List<Cliente> index() {
+        return clienteService.findAll();
     }
 
     @GetMapping("/clientes/{id}")
-    
-    public Cliente show(@PathVariable Long id){
-        return clienteService.findById(id);
+
+    public ResponseEntity<?> show(@PathVariable Long id) {
+
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            cliente = clienteService.findById(id);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al realizar la consulta en la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+        if (cliente == null) {
+            response.put("mensaje", "El cliente ID: ".concat(id.toString().concat(" no exite en la base de datos!")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
     }
 
     @PostMapping("/clientes")
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente){
+    public Cliente create(@RequestBody Cliente cliente) {
         return clienteService.save(cliente);
     }
 
     @PutMapping("/clientes/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id){
+    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
         Cliente clienteActual = clienteService.findById(id);
 
         clienteActual.setApellido(cliente.getApellido());
@@ -58,7 +79,7 @@ public class ClienteRestController {
 
     @DeleteMapping("/clientes/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id){
+    public void delete(@PathVariable Long id) {
         clienteService.delete(id);
     }
 }
